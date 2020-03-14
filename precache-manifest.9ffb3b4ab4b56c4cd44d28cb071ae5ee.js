9f6da940c94b1da768aeaca11e998565824fa739
tom <tom@sigma.edu.vn>
2020-03-14 08:44:12 +0700

    Updates

---
+++ precache-manifest.9ffb3b4ab4b56c4cd44d28cb071ae5ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3d222f48eb62ff9952a64125a9d9e8",
+    "url": "/hackernews-reader/index.html"
+  },
+  {
+    "revision": "dbdb9053ba6a3ed4c922",
+    "url": "/hackernews-reader/static/js/2.7ddb341c.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/hackernews-reader/static/js/2.7ddb341c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181695ab2d7c4cedf127",
+    "url": "/hackernews-reader/static/js/main.892166df.chunk.js"
+  },
+  {
+    "revision": "b1c698e5a5e1a6083b2b",
+    "url": "/hackernews-reader/static/js/runtime-main.6b779de2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hackernews-reader/precache-manifest.613d09f0e618be7f5e333cd69b95ea53.js"
+  "/hackernews-reader/precache-manifest.9ffb3b4ab4b56c4cd44d28cb071ae5ee.js"
 );
 
 self.addEventListener('message', (event) => {
